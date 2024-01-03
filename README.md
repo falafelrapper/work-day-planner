54d60771fdfbafc1cfd4f89096cc5bc310611940
Zachary Roy <vgd199@gmail.com>
2024-01-02 23:29:30 -0800

    Finished code, now can save to localStorage, hours change colors through time comparisons, and localStorage is received when it starts

--- README.md
+++ README.md
@@ -1,1 +1,42 @@
-# work-day-planner
+# Fullstack Development Challenge 3
+
+## Welcome to the fifth Challenge of the UCLA Bootcamp!
+
+In this challenge we were supposed to rework a starter code so that it would show a daily work planner in which you could set notes for each time slot. Depending on the time, each time slot would show a color for a "past", "present", and "future" state, corresponding with grey, red, and green. To start, we should follow our user story on what we want to solve:
+
+## User Story
+
+```
+AS AN employee with a busy schedule
+I WANT to add important events to a daily planner
+SO THAT I can manage my time effectively
+```
+
+## Acceptance Criteria
+
+```
+GIVEN I am using a daily planner to create a schedule
+WHEN I open the planner
+THEN the current day is displayed at the top of the calendar
+WHEN I scroll down
+THEN I am presented with timeblocks for standard business hours of 9am&ndash;5pm
+WHEN I view the timeblocks for that day
+THEN each timeblock is color coded to indicate whether it is in the past, present, or future
+WHEN I click into a timeblock
+THEN I can enter an event
+WHEN I click the save button for that timeblock
+THEN the text for that event is saved in local storage
+WHEN I refresh the page
+THEN the saved events persist
+```
+
+## The Completed Challenge
+
+After completing the challenge, we should now have a planner that matches the tab colors with the current time, shows the current day at the top, and allows you to save notes to your localStorage so that when you refresh, it will stay saved. You can click to view the completed site [here](https://falafelrapper.github.io/work-day-planner/)!
+
+![alt text](./assets/repo-images/site-1.jpg?raw=true "Completed Site 1")
+
+![alt text](./assets/repo-images/site-2.png?raw=true "Completed Site 2") 
+
+## Credits
+This refactor was completed by Zachary Roy
--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,23 +1,18 @@
 /* CHECKLIST FOR HW
 1X. Planner should state the current date at the top, below the main header.
-2. Below should have time blocks for 9AM-5PM
-3. Each time block should be comparing to the current time, so that it can color code to past,
+2X. Below should have time blocks for 9AM-5PM
+3X. Each time block should be comparing to the current time, so that it can color code to past,
 present, or future
-4. All time blocks should allow you to click into them, and then input info for the event of that
+4X. All time blocks should allow you to click into them, and then input info for the event of that
 time block.
-5. If you click save on the timeblock, is should save that text to localStorage
-6. All saved items should be showing even on refresh due to localStorage.
+5X. If you click save on the timeblock, is should save that text to localStorage
+6X. All saved items should be showing even on refresh due to localStorage.
 */
 
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 $(function () {
-  var hourPlanner = $('#hour');
-  var rowEl = $('div.col-2 col-md-1 hour text-center py-3').text();
-  var today = dayjs();
-  var currentDay = dayjs().format('MMMM D, YYYY');
-  $('#currentDay').text(currentDay);
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
@@ -37,17 +32,38 @@ $(function () {
   // past, present, and future classes? How can Day.js be used to get the
   // current hour in 24-hour time?
   
-  if (hourPlanner.isBefore(today)) {
-    rowEl.addClass('past');
-  } else if (hourPlanner.isSame(today)) {
-    rowEl.addClass('present');
-  } else if (hourPlanner.isAfter(today)){
-    rowEl.addClass('future')
-  }
   //
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
   //
   // TODO: Add code to display the current date in the header of the page.
+  var currentHour = dayjs().hour();
+  
+  $('.time-block').each(function () {
+    var hourPlanner = parseInt($(this).attr('id').split('-')[1]);
+    
+    if (hourPlanner < currentHour) {
+      $(this).addClass('past');
+    } else if (hourPlanner === currentHour) {
+      $(this).addClass('present');
+    } else {
+      $(this).addClass('future');
+    }
+  });
+
+  $('.saveBtn').on('click', function () {
+    var hourId = $(this).closest('.time-block').attr('id');
+    var userText = $(this).siblings('.description').val();
+    localStorage.setItem(hourId, userText);
+  });
+
+  $('.time-block').each(function () {
+    var hourId = $(this).attr('id');
+    var storedText = localStorage.getItem(hourId);
+    $(this).find('.description').val(storedText);
+  });
+
+  var currentDay = dayjs().format('MMMM D, YYYY');
+  $('#currentDay').text(currentDay);
 });
